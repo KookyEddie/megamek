2a7119462f18aad387cdde5ebb6638c7c6172d39
FASanterre <57406943+FASanterre@users.noreply.github.com>
2025-04-07 17:56:35 -0400

    Ajout tests Player & PlayerMinefieldManager

--- megamek/unittests/megamek/common/PlayerTest.java
+++ megamek/unittests/megamek/common/PlayerTest.java
@@ -19,10 +19,15 @@
 package megamek.common;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import megamek.common.icons.Camouflage;
 import org.junit.jupiter.api.Test;
 
 import megamek.client.ui.swing.util.PlayerColour;
+
+import java.util.Collection;
+import java.util.Vector;
 
 class PlayerTest {
 
@@ -39,5 +44,81 @@ class PlayerTest {
         Player player = new Player(1, playerName);
         player.setColour(PlayerColour.FUCHSIA);
         assertEquals("<B><font color='f000f0'>" + playerName + "</font></B>", player.getColorForPlayer());
+    }
+
+    @Test
+    void testGetCamouflageCategory(){
+        String playerName = "Test Player 3";
+        Player player = new Player(2, playerName);
+        Camouflage camouflage = new Camouflage(Camouflage.COLOUR_CAMOUFLAGE, PlayerColour.BLUE.name());
+        Camouflage camouflageTest = new Camouflage(Camouflage.COLOUR_CAMOUFLAGE, PlayerColour.RED.name());
+        player.setCamouflage(camouflage);
+        assertEquals(camouflage.getCategory(), player.getCamouflage().getCategory());
+    }
+
+    @Test
+    void removePlayerArty(){
+        String playerName = "Test Player 4";
+        Player player = new Player(0, playerName);
+        Vector<Coords> arty = new Vector<>();
+
+        arty.add(new Coords(1,1));
+        arty.add(new Coords(2,2));
+
+        player.setArtyAutoHitHexes(arty);
+        assertEquals(arty.size(), player.getArtyAutoHitHexes().size());
+
+        player.removeArtyAutoHitHexes();
+        assertEquals(arty.size(), player.getArtyAutoHitHexes().size());
+    }
+
+    @Test
+    void testPlayerInformations(){
+        String playerName = "Test Player 5";
+        Player player = new Player(0, "test");
+        player.setName(playerName);
+        assertEquals(playerName, player.getName());
+
+        String playerEmail = "test@gmail.com";
+        player.setEmail(playerEmail);
+        assertEquals(playerEmail, player.getEmail());
+    }
+
+    @Test
+    void testPlayerTeam(){
+        Player testPlayer = new Player(0, "test");
+        Player testPlayer2 = new Player(1, "test");
+
+        testPlayer.setTeam(Player.TEAM_NONE);
+        testPlayer2.setTeam(Player.TEAM_NONE);
+
+        assertEquals(testPlayer.getTeam(), testPlayer2.getTeam());
+    }
+
+    @Test
+    void playerIsGameMaster(){
+        Player testPlayer = new Player(0, "test");
+        testPlayer.setGameMaster(true);
+
+        assertTrue(testPlayer.getGameMaster());
+    }
+
+    @Test
+    void playerConnectedAsBot(){
+        Player testPlayer = new Player(0, "test");
+        testPlayer.setBot(true);
+
+        assertTrue(testPlayer.isBot());
+    }
+
+    @Test
+    void playerMinefield(){
+        Player testPlayer = new Player(0, "test");
+        Minefield mf = Minefield.createMinefield(new Coords(1,1), testPlayer.getId(), Minefield.TYPE_ACTIVE,
+              Minefield.MAX_DAMAGE);
+        testPlayer.minefieldManager.addMinefield(mf);
+        PlayerMinefieldManager mfManager = testPlayer.minefieldManager.copy();
+
+        assertEquals(mfManager.containsMinefield(mf), testPlayer.minefieldManager.containsMinefield(mf));
     }
 }
